dd2d7e909930381db0acf97129009ee845e7c14f
Oier Cesat <oier.cesat@gmail.com>
2022-12-07 21:58:44 +0100

    test generateurCD.php

--- generateurCD.php
+++ generateurCD.php
@@ -2,14 +2,13 @@
     define("larg_vign", 200);
     define("long_vign", 200);
 
-    header('Content-type: image/jpeg');   
+    header("Content-type: image/jpeg");   
+    $image = imageCreate(larg_vign, long_vign);
     // $image = imagecreatefromjpeg('img/pochettes/dark_bear.jpeg');       //$_GET["cheminImage"]
     // $imgReduite = imagecreatetruecolor(larg_vign, long_vign);
     // imageCopyResized($image, $imgReduite, 0,0,0,0, larg_vign, long_vign);
-
-    $image = imagecreate(larg_vign, long_fen);
-    imagefill($image,100,100,ImageColorAllocate($image,255,0,0));
-
+    $couleur = ImageColorAllocate($image,255,0,0);
+    imagefill($image,larg_vign,long_vign,$couleur);
     imagejpeg($image);
     imagedestroy($image);
 ?>
