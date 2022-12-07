666d18f745c1c111074f155f89766d29ed459520
Nicolas-Delahaie <nico601.delahaie@gmail.com>
2022-12-07 19:54:23 +0100

    Affichage des albums fonctionnel

---
+++ pagePrincipale.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Albums</title>
+        <meta name="description" content="Page principale avec chaque album">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="stylePagePrincipale.css">
+    </head>
+    <body>
+        <header>
+            <h1>Albums</h1>
+        </header>
+        <main>
+            <?php
+                $nbImages = count(glob("img/pochettes/*" ));
+                $bdd = json_decode(file_get_contents("bdd.json"));
+
+                $html = '<section id="zoneCD">';            
+                for ($i=0; $i < $nbImages; $i++) { 
+                    $cdCourant = $bdd->{"cd"}[$i];
+                    $html .= '<article>';
+                    // $html .= '<img src='.$cdCourant->{"image"}.'>';
+                    $html .= '<img src=generateurCD.php?cheminImage='.$cdCourant->{"image"}.'>';
+                    $html .= '<h2>'.$cdCourant->{"titre"}."</h2>";
+                    $html .= '<p>'.$cdCourant->{"auteur_groupe"}."</p>";
+                    $html .= '</article>';
+                }                
+                $html .= '</section>';
+
+                echo $html;
+            ?>
+        </main>
+        <footer>
+
+        </footer>
+    </body>
+</html>
