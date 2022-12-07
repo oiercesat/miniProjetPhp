ec7b49bfd5b3cdee2a2197d19f6b1cdab06a0f5c
Nicolas-Delahaie <nico601.delahaie@gmail.com>
2022-12-07 17:43:14 +0100

    Affichage d image non fonctionnel

--- generateurCD.php
+++ generateurCD.php
@@ -1,1 +1,16 @@
-<?php?>
+<?php
+    define("larg_vign", 200);
+    define("long_vign", 200);
+
+    header('Content-Type: image/jpeg');
+    $ficName = "http://localhost/HTML_CSS_PHP_JS/PHP/TP8-9/miniProjetPhp/img/pochettes/mockup.jpeg";
+    $image=imagecreatefromjpeg($ficName);      //$_GET["num_disque"]
+    // $imgReduite = imagecreatetruecolor(larg_vign, long_vign);
+    // imageCopyResized($image, $imgReduite, 0,0,0,0, larg_vign, long_vign);
+
+    // echo $image;
+    // $image = imagecreate(larg_vign, long_fen);
+    // imagefill($image,100,100,ImageColorAllocate($image,255,0,0));
+    imagejpeg($image);
+    imagedestroy($image);
+?>
