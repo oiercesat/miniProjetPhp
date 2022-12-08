57082524f742fc4daab4e14237b23936fc2f599f
Oier Cesat <oier.cesat@gmail.com>
2022-12-08 14:14:42 +0100

    Implémentation du de la page de connexion

---
+++ login.php
@@ -0,0 +1,18 @@
+<?php
+
+$idValide = 'root';
+$passwordValide = 'root';
+
+    if (isset($_POST['id']) && isset($_POST['password'])) {
+        if ($_POST['id']==$idValide && $_POST['password']=$passwordValide) {
+        session_start();
+        $_SESSION['id'] = $_POST['id'];
+        $_SESSION['password'] = $_POST['password'];
+
+        header('location: pagePrincipale.php');
+        }
+    }
+    else{
+    header('location: index.html');
+    }
+?>
