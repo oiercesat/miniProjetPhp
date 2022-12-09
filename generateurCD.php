e2f654263a90127ef7fff4ff133abccb525c3631
Nicolas-Delahaie <nico601.delahaie@gmail.com>
2022-12-09 10:06:52 +0100

    Generateur des cd fonctionnel mais en taille non reduite

--- generateurCD.php
+++ generateurCD.php
@@ -2,13 +2,12 @@
     define("larg_vign", 200);
     define("long_vign", 200);
 
-    header("Content-type: image/jpeg");   
-    $image = imageCreate(larg_vign, long_vign);
-    // $image = imagecreatefromjpeg('img/pochettes/dark_bear.jpeg');       //$_GET["cheminImage"]
-    // $imgReduite = imagecreatetruecolor(larg_vign, long_vign);
+    header("Content-type: image/png");   
+    $image = imagecreatefrompng($_GET["cheminImage"]);       //$_GET["cheminImage"]
+    $imgReduite = imagecreatetruecolor(larg_vign, long_vign);
     // imageCopyResized($image, $imgReduite, 0,0,0,0, larg_vign, long_vign);
-    $couleur = ImageColorAllocate($image,255,0,0);
-    imagefill($image,larg_vign,long_vign,$couleur);
+    // $couleur = ImageColorAllocate($image,255,0,0);
+    
     imagejpeg($image);
     imagedestroy($image);
 ?>
