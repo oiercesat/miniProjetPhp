ca9c24d0977aea38c7a0fa80dd5ad693802ace88
Nicolas-Delahaie <nico601.delahaie@gmail.com>
2022-12-12 22:10:00 +0100

    Transformation du .html en .php

---
+++ src/pagePaiement.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Paiement</title>
+        <meta name="description" content="Page de paiement du disque">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="styles/pagePaiement.css">
+    </head>
+    <body>
+        
+
+    <?php
+        //Apres l achat, on retire tous les articles du panier
+        session_unset ();
+    ?>
+    </body>
+</html>
