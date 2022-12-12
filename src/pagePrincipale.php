d43a437eba6ab634d1af9f664c93cea6428f7dfd
Nicolas-Delahaie <nico601.delahaie@gmail.com>
2022-12-12 18:20:13 +0100

    ajout validation du panier et suppression de containerZoneCD

--- src/pagePrincipale.php
+++ src/pagePrincipale.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -7,6 +8,8 @@
         <meta name="description" content="Page principale avec chaque album">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="styles/stylePagePrincipale.css">
+        <script src="scripts/modifPanier.js" async defer></script>
+        
     </head>
     <body>
         <header>
@@ -17,19 +20,28 @@
                 $nbImages = count(glob("img/pochettes/*" ));
                 $bdd = json_decode(file_get_contents("datas/bdd.json"));
 
-                $html = '<section id="containerZoneCD"><section id="zoneCD">';            
+                $html = '<section id="zoneCD">';            
                 for ($i=0; $i < $nbImages; $i++) { 
                     $cdCourant = $bdd->{"cd"}[$i];
                     $html .= '<article>';
                     $html .= '<img src=scripts/generateurCD.php?cheminImage='.$cdCourant->{"image"}.'>';                  
                     $html .= '<h2>'.$cdCourant->{"titre"}."</h2>";
                     $html .= '<p>'.$cdCourant->{"auteur_groupe"}."</p>";
-                    $html .= '</article>';
+                    $html .= '<section class="zoneAjoutPanier">';
+                    $html .= '<button class="btnAjouterPanier btnPanier">+</button>';
+                    $html .= '<p class="nbArticlesPanier"> 0 </p>';
+                    $html .= '<button class="btnRetirerPanier btnPanier">-</button>';
+                    $html .= '</section></article>';
                 }                
-                $html .= '</section></section>';
-
+                $html .= '</section>';
                 echo $html;
             ?>
+
+            <nav id="zoneValidationPanier">
+                <button id="bValidationPanier">Valider le panier</button>
+            </nav>
+
+            
         </main>
         <footer>
 
