a55965b74019adf562e27fba24d4491abfee3ee1
Nicolas-Delahaie <nico601.delahaie@gmail.com>
2022-12-14 20:39:12 +0100

    Commentaires + derniers correctifs

--- src/scripts/majArticlesPanier.php
+++ src/scripts/majArticlesPanier.php
@@ -1,10 +1,17 @@
 <?php
+/**
+ * @brief Script ajoutant ou supprimant un element de la base de donnee (bdd.json)
+ * @param id numero de l image dans la bdd 
+ * @param action add ou supp en fonction de ce qu on veut faire
+ */
     if (isset($_GET["id"]) and isset($_GET["action"]))
     {
+        //Si les deux parametres sont bien definis
         $lienDonnees = "../datas/bdd.json";
         $bdd = json_decode(file_get_contents($lienDonnees));
         $idArticle = strval($_GET["id"]);
         $action = $_GET["action"];
+        /** @warning Il faut retirer "Nico" et faire marrcher les variables de session*/
         $utilisateur = "Nico"; //$_SESSION["user"];
 
         if ($action == "add")
@@ -17,7 +24,6 @@
         {
             //Recherche de l article
             $emplacementArticle =  array_search($idArticle, $bdd->{"utilisateurs"}->{$utilisateur}->{"panier"});
-            echo $emplacementArticle;
             if ($emplacementArticle == false)
             {
                 //Si l article existe suppression puis tri
@@ -29,7 +35,7 @@
         $newFile = json_encode($bdd);
         file_put_contents($lienDonnees, $newFile);
     }
+    /** @warning Lien non dynamique */
     header("Location: http://localhost/PROGRAMMATION/HTML_CSS_PHP_JS/PHP/TP8-9/miniProjetPhp/src/pagePrincipale.php");
-    // echo "coucou";
     
 ?>
