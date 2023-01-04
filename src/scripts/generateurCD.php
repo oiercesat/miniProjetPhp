37fd2ee3c89dcb33d30c82078a0d3d2790cb299b
Nicolas-Delahaie <nico601.delahaie@gmail.com>
2023-01-04 21:52:35 +0100

    Correction problemes

--- src/scripts/generateurCD.php
+++ src/scripts/generateurCD.php
@@ -3,13 +3,13 @@
     define("long_vign", 150);
 
     header("Content-type: image/png");
-    $src = '../'.$_GET["cheminImage"];
+    $src = '../datas/img/pochettes/'.$_GET["nomImage"];
     $image = imagecreatefrompng($src);
     $imgReduite = imagecreatetruecolor(larg_vign, long_vign);
     $ancienneDimensions = getimagesize($src);
     imageCopyResized($imgReduite, $image, 0,0,0,0, larg_vign, long_vign, $ancienneDimensions[0], $ancienneDimensions[1]);
     
     imagepng($imgReduite);
-    imagedestroy($imgage);
+    imagedestroy($image);
     imagedestroy($imgReduite);
 ?>
