182ce6ece363ed50ff531a4895113a29198a2b7c
Nicolas-Delahaie <nico601.delahaie@gmail.com>
2023-01-04 10:39:39 +0100

    Adaptation du code a la nouvelle bd

--- src/scripts/verifLogin.php
+++ src/scripts/verifLogin.php
@@ -4,8 +4,8 @@ $bdd= json_decode(file_get_contents("./../datas/bdd.json"));
 $utilisateurs = $bdd->{"utilisateurs"};
 
 if (isset($_POST['id']) && isset($_POST['password'])) {    
-    foreach ($utilisateurs as $utilisateur) {
-        if ($utilisateur->{"id"} == $_POST['id'] && $utilisateur->{"pass"} == $_POST['password']) {
+    foreach ($utilisateurs as $utilisateur => $informations) {
+        if ($utilisateur == $_POST['id'] && $informations->{"pass"} == $_POST['password']) {
             session_start();
             $_SESSION["id"] = $_POST['id'];
             header("Location: ./../pagePrincipale.php");
